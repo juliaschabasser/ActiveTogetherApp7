2e41faab8106f3faf07cc86618b6ce49bb2c390a
wagne <2310859010@fh-burgenland.at>
2024-12-15 19:40:02 +0100

    feat:

--- src/app/dashboard/add-data/add-data.component.ts
+++ src/app/dashboard/add-data/add-data.component.ts
@@ -1,22 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { SharedModule } from '../../shared/shared.module';
-import { Validators, FormBuilder } from '@angular/forms';
-import { StoreService } from '../../shared/store.service';
-import { BackendService } from '../../shared/backend.service';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {StoreService} from '../../shared/store.service';
+import {BackendService} from '../../shared/backend.service';
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatButtonModule} from "@angular/material/button";
+
 @Component({
   selector: 'app-add-data',
-  standalone: true,  // standalone-Komponente
-  imports: [SharedModule],  // Import der benötigten Module
   templateUrl: './add-data.component.html',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatButtonModule
+  ],
   styleUrls: ['./add-data.component.css']
 })
 export class AddDataComponent implements OnInit {
-  constructor(private formbuilder: FormBuilder, public storeService: StoreService, private backendService: BackendService) {
+  public registrationForm!: FormGroup;
+  public showToast = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public storeService: StoreService,
+    private backendService: BackendService
+  ) {
   }
-  public registrationForm: any;
 
   ngOnInit(): void {
-    this.registrationForm = this.formbuilder.group({
+    this.registrationForm = this.formBuilder.group({
       name: ['', [Validators.required]],
       courseId: ['', Validators.required],
       birthdate: [null, Validators.required]
@@ -24,8 +40,19 @@ export class AddDataComponent implements OnInit {
   }
 
   onSubmit() {
-    if(this.registrationForm.valid) {
+    if (this.registrationForm.valid) {
       this.backendService.addRegistration(this.registrationForm.value, this.storeService.currentPage);
+
+
+      this.showToast = true;
+
+
+      setTimeout(() => {
+        this.showToast = false;
+      }, 3000);
+
+
+      this.registrationForm.reset();
     }
   }
 }
